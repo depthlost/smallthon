426a3cbff73a46cdbfca70a4827905ed7ea8a6db
depthlost <iyaelpereyra@gmail.com>
2020-10-19 19:25:31 -0300

    An import error was fixed

--- smallthon/smallthon.py
+++ smallthon/smallthon.py
@@ -1,6 +1,6 @@
 import inspect
 from forbiddenfruit import curse
-import smallthon.collections.smallthon_list
+from smallthon.collections import smallthon_list
 
 def sm_list():
     for each in [func for func in inspect.getmembers(smallthon_list, predicate=inspect.isfunction)]:
