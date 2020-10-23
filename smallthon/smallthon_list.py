3601a30db3a4caea9cbb5a098deeb3d0cbaf2667
PEREYRA, NEHUEN <nehuenpereyra@gmail.com>
2020-10-23 16:15:13 -0300

    Added reject, collect, do, is_empty, flatten, remove_duplicated, flat_collect, inject, includes, any_satisfy and all_satisfy methods to smallthon_list module

--- smallthon/smallthon_list.py
+++ smallthon/smallthon_list.py
@@ -26,3 +26,49 @@ def detect(self, function, default=None):
 
 def select(self, function):
     return [ each for each in self if function(each) ]
+
+def reject(self, function):
+    return [ each for each in self if not function(each) ]
+
+def collect(self, function):
+    return [ function(each) for each in self ]
+
+def do(self, function):
+    for each in self: function(each) 
+
+def is_empty(self):
+    return self.size() == 0
+
+def flatten(self):
+    return [ item for each in self for item in each ]
+
+def remove_duplicated(self):
+    result_list = []
+    for each in self: 
+        if each not in result_list: 
+            result_list.append(each)
+    return result_list
+
+def flat_collect(self, function):
+    return self.collect(function).flatten()
+
+def inject(self, function, initial_value):
+    result = initial_value
+    for each in self:
+        result = function(each, result)
+    return result
+
+def includes(self, element):
+    return element in self
+
+def any_satisfy(self, function):
+    for each in self:
+        if function(each):
+            return True
+    return False
+
+def all_satisfy(self, function):
+    for each in self:
+        if not function(each):
+            return False
+    return True
