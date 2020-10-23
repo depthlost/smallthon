7da2aca6e604188038b455d3177203bfc240d776
depthlost <iyaelpereyra@gmail.com>
2020-10-23 12:29:56 -0300

    Added detect and select methods to smallthon_list module

--- smallthon/smallthon_list.py
+++ smallthon/smallthon_list.py
@@ -3,3 +3,13 @@ def first(self):
     
 def last(self):
     return self[-1]
+
+def detect(self, function, default=None):
+    for each in self:
+        if (function(each)):
+            return each
+    return default
+
+def select(self, function):
+    return [ each for each in self if function(each) ]
+    
