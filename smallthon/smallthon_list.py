b1bc0bbdfd156eb0218fcfa89a8c6a661337940b
depthlost <iyaelpereyra@gmail.com>
2020-10-23 12:45:35 -0300

    Added add, add_first, add_last and size methods to smallthon_list module

--- smallthon/smallthon_list.py
+++ smallthon/smallthon_list.py
@@ -4,6 +4,20 @@ def first(self):
 def last(self):
     return self[-1]
 
+def add(self, element):
+    return self.add_last(element)
+
+def add_first(self, element):
+    self.insert(0, element)
+    return element
+
+def add_last(self, element):
+    self.append(element)
+    return element
+
+def size(self):
+    return len(self)
+
 def detect(self, function, default=None):
     for each in self:
         if (function(each)):
@@ -12,4 +26,3 @@ def detect(self, function, default=None):
 
 def select(self, function):
     return [ each for each in self if function(each) ]
-    
